7a50670b1ec3058ab8c151c70c97da6f04dd4d25
Caio Alexandre <caioalexandre.br@hotmail.com>
2017-09-03 13:42:24 -0300

    Ajuste PreLoader

--- js/changeColors.js
+++ js/changeColors.js
@@ -1,12 +1,14 @@
 // change color button
+
     $(".seleciona-turquoise").on("click", function(e){
         $("link").attr("href", "style.css");
     });
-
+// CSS Turquoise
     $(".seleciona-red").on("click", function(e){
         $("link").attr("href", "css/style_red.css");
     });
-
+// CSS Red
     $(".seleciona-purple").on("click", function(e){
         $("link").attr("href", "css/style_purple.css");
     });
+// CSS Purple
