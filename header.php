1778b3106fad60a5f09f09acc078da0cc2903199
Caio Alexandre <caioalexandre.br@hotmail.com>
2017-09-03 15:56:13 -0300

    ng-include header

---
+++ header.php
@@ -0,0 +1,24 @@
+<header class="bgParallax" data-speed="10">
+    <nav>
+        <div class="bt-logo">
+            <a href="#" title="START" >LOGO</a>
+        </div>
+        <div class="menu">
+            <li>
+                <ul><a href="#" title="START" class="active" >START</a></ul>
+                <ul><a href="#" title="ABOUT ME" >ABOUT ME</a></ul>
+                <ul><a href="#" title="WORK" >WORK</a></ul>
+                <ul><a href="#" title="CONTACT" >CONTACT</a></ul>
+            </li>
+        </div>
+    </nav>
+    <div class="header-int">
+        <h1>May I introduce myself?</h1>
+        <div class="div"></div>
+        <h2>Frontend Design | UI/UX Design</h2>
+        
+        <div id="header-bt">
+            <a href="#" title="GO AHEAD" >GO AHEAD</a>
+        </div>
+    </div>
+</header>
