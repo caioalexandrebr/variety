e5863ca7034addef01f7753e6b493a42f05fed8b
Caio Alexandre <caioalexandre.br@hotmail.com>
2017-09-03 13:33:38 -0300

    Ajuste changeColors.js

--- js/changeColors.js
+++ js/changeColors.js
@@ -1,15 +1,12 @@
 // change color button
     $(".seleciona-turquoise").on("click", function(e){
-         e.preventDefault();
         $("link").attr("href", "style.css");
     });
 
     $(".seleciona-red").on("click", function(e){
-         e.preventDefault();
         $("link").attr("href", "css/style_red.css");
     });
 
     $(".seleciona-purple").on("click", function(e){
-         e.preventDefault();
         $("link").attr("href", "css/style_purple.css");
     });
