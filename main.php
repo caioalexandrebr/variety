ac0e6ead43759eec50d3bb0d7ad8ef3f3f596dd2
Caio Alexandre <caioalexandre.br@hotmail.com>
2017-09-03 16:03:20 -0300

    ng-include main

---
+++ main.php
@@ -0,0 +1,44 @@
+<div class="div_article">
+        <div class="div_article_left">
+            <article>
+                <a href="#" title="Lorem ipsum dolor sit" class="article-search"></a>
+                <div class="article-ft">
+                    <a href="#" title="Lorem ipsum dolor sit" >Lorem ipsum dolor sit</a>
+                    <h2>amet consetetur sadipscing elitr</h2>
+                </div>
+                <div class="article-img first-ficure"></div>
+            <!-- <div class="article-img"><img src="/figure.jpg"></div> -->
+            </article>
+            <article>
+                <a href="#" title="Lorem ipsum dolor sit" class="article-search"></a>
+                <div class="article-ft">
+                    <a href="#" title="Lorem ipsum dolor sit" >Lorem ipsum dolor sit</a>
+                    <h2>amet consetetur sadipscing elitr</h2>
+                </div>
+                <div class="article-img second-ficure"></div>
+            <!-- <div class="article-img"><img src="/figure.jpg"></div> -->
+            </article>
+        </div>
+        <div class="div_article_right">
+            <article>
+                <a href="#" title="Lorem ipsum dolor sit" class="article-search"></a>
+                <div class="article-ft">
+                    <a href="#" title="Lorem ipsum dolor sit" >Lorem ipsum dolor sit</a>
+                    <h2>amet consetetur sadipscing elitr</h2>
+                </div>
+                <div class="article-img third-ficure"></div>
+            <!-- <div class="article-img"><img src="/figure.jpg"></div> -->
+            </article>
+            <article>
+                <a href="#" title="Lorem ipsum dolor sit" class="article-search"></a>
+                <div class="article-ft">
+                    <a href="#" title="Lorem ipsum dolor sit" >Lorem ipsum dolor sit</a>
+                    <h2>amet consetetur sadipscing elitr</h2>
+                </div>
+                <div class="article-img fourth-ficure"></div>
+            <!-- <div class="article-img"><img src="/figure.jpg"></div> -->
+            </article>
+        </div>
+    </div>
+
+    <div class="div_article_viewmore"><a href="#">View More</a></div>
