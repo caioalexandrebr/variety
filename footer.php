45a4db93826a9f32ec67d380cbdd7d5b26733414
Caio Alexandre <caioalexandre.br@hotmail.com>
2017-09-03 15:54:25 -0300

    ng-include footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="div_footer_center">
+        <div class="div_footer_center_sociais">
+            <div class="div_footer_center_sociais_area">
+                <div id="dribbble"><a href="#"><div class="dribbble"></div></a></div>
+                <div id="twitter"><a href="#"><div class="twitter"></div></a></div>
+                <div id="facebook"><a href="#"><div class="facebook"></div></a></div>
+            </div>
+        </div>
+        <div class="footer_autor">© Caio Alexandre - <a href="https://github.com/caioalexandrebr" alt="GitHub">GitHub</a> - <a href="https://www.linkedin.comin/caioalexandrebr/" alt="Linkedin">Linkedin</a></div>
+    </div>
+</footer>
